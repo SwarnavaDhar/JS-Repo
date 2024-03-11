43ea47c1d306d40a5e914f177cfe8dddaeb30bc5
swarnavadhar2000@gmail.com <swarnavadhar2000@gmail.com>
2024-03-11 14:40:28 +0530

    added file 11 and 12

--- 11_Array.js
+++ 11_Array.js
@@ -22,17 +22,58 @@
 // console.log(typeof newArr)
 
 //Slice --> Basically creates a copy of the and slices it
-const arr1 = [10,20,30,40,50,60]
-console.log("Original Array: ", arr1)
-console.log(typeof arr1) 
-console.log("Sliced Array: " , arr1.slice(0,3)) // includes the start index and excludes the last index , Output => [10,20,30]
+// const arr1 = [10,20,30,40,50,60]
+// console.log("Original Array: ", arr1)
+// console.log(typeof arr1) 
+// console.log("Sliced Array: " , arr1.slice(0,3)) // includes the start index and excludes the last index , Output => [10,20,30]
 
-console.log("*************************************************************************************************************")
+// console.log("*************************************************************************************************************")
 
 //  Splice --> Manipulates the original array
-console.log("Original Array: ", arr1)
-console.log("Spliced array: ", arr1.splice(0,2)) 
-console.log("After splicing original array: ",arr1)
+// console.log("Original Array: ", arr1)
+// console.log("Spliced array: ", arr1.splice(0,2)) 
+// console.log("After splicing original array: ",arr1)
+
+// console.log("***********************************************Array Part 2**************************************************************")
+
+const marvel = ["Thor","Iron-Man","Spiderman"]
+const dc = ["Batman","Superman","Flash"]
+// marvel.push(dc) // It takes every element of marvel array as a single element and takes the dc array as a one whole element
+// console.log(marvel)
+
+// const allHero = marvel.concat(dc) // concats both the arrays into a single array and retruns a new array
+// console.log(allHero)
+
+// const AllHeroes = [...marvel, ...dc] // (... => Spread Operator, concats both the arrays into a single array and retruns a new array)
+// console.log(AllHeroes)
+
+const numArray = [1,2,3,[4,5],6,[7,[8,9,10]]]
+const flatArray = numArray.flat(2) // flat basically flatens all arrays of array into a new single array with each element coma separated and we can also add depth within brackets (like ---> 0,1,2) OR we can write "Infinite" to flatens every arrays of array // Eg - [1,2,3,[4,5],6,[7,[8,9,10]]] --> It has depth 2, because it has array in the array, in the another array.
+// console.log(flatArray)
+
+// isArray =>checks whether the passed value is in the Array or not and based on that returns a boolean value.
+// console.log(Array.isArray("Hitesh")) // Output => false
+// console.log(Array.isArray(["Swarnava"])) // Output => true, as "Swarnava" is in third bracket means array
+
+// console.log(Array.from("Swarnava")) // Turns every single alphabet, a single element into an array
+
+// console.log(Array.from({name: "Swarnava"})) // It will return an empty array
+
+let score = [100,200,300]
+let score1 = [400,500,600]
+let score2 = [700,800,900]
+console.log(Array.of(score,score1,score2)) // It will return an array which will conatain three array
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 12_objects.js
@@ -0,0 +1,91 @@
+// Object Literals
+
+// const user = {} // creating an empty object, Is a Singleton Object
+// const user2 = new Object () // Another way of creating an object, It is non-singleton object
+
+const mySym = Symbol("Key1")
+
+//creating an object
+const user = {
+    name: "Swarnava",
+    age: 23,
+    [mySym] : "myKey1", // Write way of declaring symbol, symbol variable should be in 3rd bracket
+    Location: "Kolkata",
+    email: "sdk@gmail.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday","Saturday"] // declaring objects
+}
+
+
+//Now accessing the objects: 
+// Type 1:
+// console.log(user.age) // acces values using (.) operator [Not a good practice]
+
+// Type 2:
+// console.log(user["age"]) // Another way of accessing value 
+
+// console.log(user[mySym]) //Acessing symbol variable
+
+// console.log(user.email) // Print old email
+// console.log(user.email = "sdk2000@gmail.com") // changing the existing email, sets and prints new email
+
+// Object.freeze(user) // Here its freezing the email
+// user.email = "sdhar@gmail.com" // Although we are trying to change the email value, it will no change as it's frozen
+// console.log(user["email"]) // Will print the old mail
+
+// console.log(user)
+
+//function creation
+
+// user.greeting = function(){
+//     console.log("Hello Duniya") 
+// }
+// console.log(user.greeting()) // [Function (anonymous)]
+// console.log(user.greeting()) // Output => Undefined
+
+// user.greeting2 = function(){
+//     console.log(`Hello ${this.name}, your age is ${this.age}`)
+// }
+// console.log(user.greeting2) // [Function (anonymous)]
+// console.log(user.greeting2()) // Output => Hello Swarnava(as we are accessing name using "this"), your age is 23(as we are accessing age using "this")
+
+
+// console.log("***********************************************Objects Part 2**************************************************************")
+// creating objects of objects
+
+const regularUser = {
+    email: "sdk@gmail.com",
+    name: {
+        fullName: {
+            firstName: "Swarnava",
+            lastname: "Dhar",
+        }
+    },
+    isLoggedIn: "false"
+}
+// console.log(regularUser)
+// console.log(regularUser.name.fullName.firstName) // Accessing multiple levels of objects, Output => Swarnava
+
+const obj1 = {1: "Ayan", 2: "Bantayan"} // key value pair, 1 --> key, "Ayan" --> value
+const obj2 = {3: "Ayushi", 4: "Samiran"} 
+const mergedObj = Object.assign(obj1, obj2) // for assigning object
+// console.log(mergedObj)
+const MergedObj = {...obj1, ...obj2} // Using Spread operator
+// console.log(MergedObj)
+
+const users = [
+    {
+        id: 1,
+        email: "s@gmail.com"
+    },
+    {
+        id: 1,
+        email: "s@gmail.com"
+    }
+]
+users[1].email
+// console.log(Object.keys(regularUser)) // Output => [ 'email', 'name', 'isLoggedIn' ], prints the keys in an array
+// console.log(Object.values(regularUser)) // prints the values in an array
+// console.log(Object.entries(regularUser)) // Prints each key-value pair in a separate array
+console.log(regularUser.hasOwnProperty('isLoggedIn')) // checks whethet a property present or not and gives a bool value based on that
+
